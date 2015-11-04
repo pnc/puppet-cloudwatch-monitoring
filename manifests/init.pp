3f70bcd117fc931132d01befeb601ca38719bc0f
Phil Calvin <phil@philcalvin.com>
2015-11-03 21:55:36 -0500

    Proper invocation for cron

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,16 @@
-class monitored($ensure = present) {
-  class { 'awscli': }
-
+define cloudwatch::monitored($ensure = present) {
   file { 'monitoring-script':
     ensure => $ensure,
     path => "/usr/local/bin/aws-mon",
     mode => 0755,
-    content => template("cloudwatch-monitoring/aws-mon-linux/aws-mon.sh"),
+    source => "puppet:///modules/cloudwatch/aws-mon-linux/aws-mon.sh",
     require => File['/root/.aws/credentials'],
   }
 
   cron { 'report-metric':
     ensure  => $ensure,
-    command => "/usr/local/bin/aws-mon --dimensions server=lol --disk-space-util",
+    command => "/usr/local/bin/aws-mon --namespace Detail --dimensions server=${hostname} --region us-east-1 --disk-space-util --disk-path /",
+    environment => ['HOME=/root', 'PATH=/usr/local/bin:/usr/bin:/bin'],
     user    => 'root',
     hour    => '*',
     minute  => '*/5',
